ae0cab94c7f4d42b4d975f0be8c3fd9077c675dc
Fan Hongtao <fanhongtao@gmail.com>
2013-12-22 17:35:21 +0800

    Code from branch: ics-mr0-release

--- src/android/util/TimeUtils.java
+++ src/android/util/TimeUtils.java
@@ -9,14 +9,14 @@ package android.util;
 public class TimeUtils {
     /** @hide Field length that can hold 999 days of time */
     public static final int HUNDRED_DAY_FIELD_LEN = 19;
-    
+
     private static final int SECONDS_PER_MINUTE = 60;
     private static final int SECONDS_PER_HOUR = 60 * 60;
     private static final int SECONDS_PER_DAY = 24 * 60 * 60;
 
     private static final Object sFormatSync = new Object();
     private static char[] sFormatStr = new char[HUNDRED_DAY_FIELD_LEN+5];
-    
+
     static private int accumField(int amt, int suffix, boolean always, int zeropad) {
         if (amt > 99 || (always && zeropad >= 3)) {
             return 3+suffix;
@@ -29,7 +29,7 @@ public class TimeUtils {
         }
         return 0;
     }
-    
+
     static private int printField(char[] formatStr, int amt, char suffix, int pos,
             boolean always, int zeropad) {
         if (always || amt > 0) {
@@ -53,24 +53,24 @@ public class TimeUtils {
         }
         return pos;
     }
-    
+
     private static int formatDurationLocked(long duration, int fieldLen) {
         if (sFormatStr.length < fieldLen) {
             sFormatStr = new char[fieldLen];
         }
-        
+
         char[] formatStr = sFormatStr;
-        
+
         if (duration == 0) {
             int pos = 0;
             fieldLen -= 1;
             while (pos < fieldLen) {
-                formatStr[pos] = ' ';
+                formatStr[pos++] = ' ';
             }
             formatStr[pos] = '0';
             return pos+1;
         }
-        
+
         char prefix;
         if (duration > 0) {
             prefix = '+';
@@ -97,7 +97,7 @@ public class TimeUtils {
         }
 
         int pos = 0;
-        
+
         if (fieldLen != 0) {
             int myLen = accumField(days, 1, false, 0);
             myLen += accumField(hours, 1, myLen > 0, 2);
@@ -110,10 +110,10 @@ public class TimeUtils {
                 myLen++;
             }
         }
-        
+
         formatStr[pos] = prefix;
         pos++;
-        
+
         int start = pos;
         boolean zeropad = fieldLen != 0;
         pos = printField(formatStr, days, 'd', pos, false, 0);
@@ -124,7 +124,7 @@ public class TimeUtils {
         formatStr[pos] = 's';
         return pos + 1;
     }
-    
+
     /** @hide Just for debugging; not internationalized. */
     public static void formatDuration(long duration, StringBuilder builder) {
         synchronized (sFormatSync) {
